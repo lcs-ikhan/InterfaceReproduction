04452ab77d3bb7345ef5ab3420d122ee8ebef47a
Isaad Khan <IKhan@lcs.on.ca>
2022-11-16 11:55:39 -0500

    Initial Commit

---
+++ InterfaceReproduction/InterfaceReproductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceReproductionApp.swift
+//  InterfaceReproduction
+//
+//  Created by Isaad Khan on 2022-11-16.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceReproductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
