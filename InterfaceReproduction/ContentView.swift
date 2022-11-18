c244a5b5724c53eede555ba02b8632a3c52ef982
Isaad Khan <IKhan@lcs.on.ca>
2022-11-17 20:27:32 -0500

    Made the circle and checkmark

--- InterfaceReproduction/ContentView.swift
+++ InterfaceReproduction/ContentView.swift
@@ -9,15 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            Circle()
+                .foregroundColor(.green)
+                .frame(width: 100)
+            
+            Circle()
+                .foregroundColor(.black)
+                .frame(width:93)
+            
+            Image(systemName: "checkmark")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.green)
+                .frame(width: 50)
+            
+               
         }
-        .padding()
+    
     }
+
+    
 }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
